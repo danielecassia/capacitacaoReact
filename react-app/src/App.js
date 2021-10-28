059b0b52c1e97db43caacdf4a538a9446c20da03
Daniele Cassia <danielecassia9@gmail.com>
2021-10-28 20:25:34 -0300

    edição no template

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import Cadastro from './componentes/cadastro/Cadastro';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- react-app/src/componentes/cadastro/Cadastro.jsx
+++ react-app/src/componentes/cadastro/Cadastro.jsx
@@ -1,13 +1,26 @@
 import './Cadastro.css';
 import FormCadastro from './formCadastro/formCadastro';
-
+import Logo from "../../logo.svg";
 export default function Login() {
   return(
     <div className="Cadastro">
-      <h1>Cadastro</h1>
-      <FormCadastro>
+      <div className="conteudo">
+      </div>
+      <div className="left-side">
+          <img src={Logo}
+          alt="seta"
+          width="70%"
+          height="70%"
+          padding="0" 
+            />
+      </div>
 
-      </FormCadastro>
+      <div className="right-side">
+        <h1>Cadastro</h1>
+        <FormCadastro/>
+      </div>
+
+
     </div>
   )
 }
